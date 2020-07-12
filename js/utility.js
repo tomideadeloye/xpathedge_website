e0301d7a204959efff35e0817adfd685c755549a
Olufemi Daniel <oladotun.olufemi@yahoo.com>
2020-07-12 20:00:44 +0100

    work updates

--- js/utility.js
+++ js/utility.js
@@ -1,19 +1,3 @@
-barba.init({
-    transitions: [{
-        name: 'opacity-transition',
-        leave(data) {
-            return gsap.to(data.current.container, {
-                opacity: 0
-            });
-        },
-        enter(data) {
-            return gsap.from(data.next.container, {
-                opacity: 0
-            });
-        }
-    }]
-});
-
 $(window).scroll(function() {
 
     // selectors
@@ -45,3 +29,65 @@ $(window).scroll(function() {
     });
 
 }).scroll();
+
+
+$(document).scroll(function () {
+    var y = $(this).scrollTop();
+
+    if (y > 840 && y <= 1450) {
+        $('.social_share').addClass('add_fixed');
+    } else {
+        $('.social_share').removeClass('add_fixed');
+    }
+
+    console.log(y)
+})
+
+
+function pageTransition() {
+
+    var tl = gsap.timeline();
+
+    tl.to('ul.transition li', {duration: .5, scaleY: 1, transformOrigin: "bottom left", stagger: .2})
+
+    tl.to('ul.transition li', {duration: .5, scaleY: 0, transformOrigin: "bottom left", stagger: .1, delay: .1})
+
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+}
+
+
+function delay(n) {
+    n = n || 2000;
+    return new Promise(done => {
+        setTimeout(() => {
+            done();
+        }, n)
+    })
+}
+
+if (typeof $("div").data('barba') !== 'undefined') {
+    barba.init({
+        sync: true,
+        transitions: [{
+            sync: true,
+            name: 'opacity-transition',
+            async leave(data) {
+                const done =  this.async();
+
+                pageTransition();
+                await delay(1500);
+                done()
+            },
+        }]
+    });
+
+    barba.hooks.after(() => {
+        AOS.init();
+    });
+}
+
+
+
+
+
+
